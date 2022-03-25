c33036f947f1ef26f2eb1f17a1dc9f9de201bb6b
Robert Gray <rgray237@gmail.com>
2022-03-25 08:54:06 -0500

    First steps at navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import * as boostrap from 'boostrap';
 
+/**
+   * Navbar links active state on scroll
+   */
+ let navbarlinks = select('#navbar .scrollto', true)
+ const navbarlinksActive = () => {
+   let position = window.scrollY + 200
+   navbarlinks.forEach(navbarlink => {
+     if (!navbarlink.hash) return
+     let section = select(navbarlink.hash)
+     if (!section) return
+     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+       navbarlink.classList.add('active')
+     } else {
+       navbarlink.classList.remove('active')
+     }
+   })
+ }
+ window.addEventListener('load', navbarlinksActive)
+ onscroll(document, navbarlinksActive)
+
 /*
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xff0000 );
